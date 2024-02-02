8d5585a70d1b795aea44bc900ef9b3b7f10dc2d2
Paulo Vaz <pvaz1006@gmail.com>
2024-02-02 00:58:42 -0300

    first commit

---
+++ app/src/main/java/com/example/multiplelanguage/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.multiplelanguage
+
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity : AppCompatActivity() {
+    lateinit var item: EditText
+    lateinit var add: Button
+    lateinit var listView: ListView
+
+    var itemList = ArrayList<String>()
+    var fileHelper = FileHelper()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        item = findViewById(R.id.editText)
+        add = findViewById(R.id.btnAdd)
+        listView = findViewById(R.id.list)
+        itemList = fileHelper.readData(this)
+
+        var arrayAdapter =  ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1, itemList)
+
+        listView.adapter = arrayAdapter
+
+        add.setOnClickListener {
+            var itemName : String = item.text.toString()
+            if (itemName.length === 0) {
+                Toast.makeText(applicationContext,"Value is empty", Toast.LENGTH_LONG).show()
+            } else {
+                itemList.add(itemName)
+                item.setText("")
+
+                fileHelper.writeData(itemList, applicationContext)
+                arrayAdapter.notifyDataSetChanged()
+            }
+        }
+
+        listView.setOnItemClickListener {adapterView, view, position, l ->
+            var alert = AlertDialog.Builder(this)
+            alert.setTitle("Delete")
+            alert.setMessage("Do you wante delete this item form the list?")
+            alert.setCancelable(false)
+
+            alert.setNegativeButton("No",  DialogInterface.OnClickListener {
+                dialogInterface, i -> dialogInterface.cancel()
+            })
+
+            alert.setPositiveButton("Yes", DialogInterface.OnClickListener {
+                dialogInterface, i ->
+                itemList.removeAt(position)
+                arrayAdapter.notifyDataSetChanged()
+                fileHelper.writeData(itemList, applicationContext)
+            })
+
+            alert.create().show()
+        }
+    }
+}
